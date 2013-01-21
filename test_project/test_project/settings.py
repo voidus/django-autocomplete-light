ac4c2909e59b7d5404b14a51e3892223960f8d22
jpic <jamespic@gmail.com>
2013-01-21 18:06:55 +0100

    Use django-admin-bootstrapped in test_project demo

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -138,6 +138,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'cities_light',
     'table_autocomplete',
